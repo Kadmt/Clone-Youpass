8f980e8f8a17c39fca0e9df0d7877922da431cd0
Kad <Khonganhduy123@gmail.com>
2026-09-09 16:50:02 +0700

    chore: fix the OnetoOne relation between feedback and submission

--- Backend/src/main/java/com/youpass/backend/entity/Submission.java
+++ Backend/src/main/java/com/youpass/backend/entity/Submission.java
@@ -1,37 +1,39 @@
 package com.youpass.backend.entity;
 
-
 import jakarta.persistence.*;
-
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "submissions")
+@Getter
+@Setter
 public class Submission {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne
-    @JoinColumn( name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(name = "skill_type")
-    private String skill_type;
+    private String skillType;
 
-    @Column( name = "reference_id")
-    private long referenceId;
+    @Column(name = "reference_id")
+    private Long referenceId;
 
-    @Column( name = "answer_data", columnDefinition = "TEXT")
+    @Column(name = "answer_data", columnDefinition = "TEXT")
     private String answerData;
 
-    @Column( name = "score")
-    private long score;
+    @Column(name = "score")
+    private Integer score;
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-
+    @OneToOne(mappedBy = "submission")   // lấy feedback qua submission - không cần tách riêng thành feedbackRepo
+    private WritingFeedback feedback;
 }
