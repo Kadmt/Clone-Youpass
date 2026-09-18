c886ceec98a47daa18b076a96362cad798bd9760
Kad <Khonganhduy123@gmail.com>
2026-09-18 23:59:13 +0700

    fix(repository): add @Repository annotation to ReadingPassageRepository

--- Backend/src/main/java/com/youpass/backend/repository/reading/ReadingPassageRepository.java
+++ Backend/src/main/java/com/youpass/backend/repository/reading/ReadingPassageRepository.java
@@ -2,7 +2,10 @@ package com.youpass.backend.repository.reading;
 
 import com.youpass.backend.entity.ReadingPassage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface ReadingPassageRepository extends JpaRepository<ReadingPassage, Long> {
 
     // các hàm cơ bản có sẵn
