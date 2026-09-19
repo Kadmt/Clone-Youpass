245a799a8c14d9b5881109b42c93824c3894c7b6
Kad <Khonganhduy123@gmail.com>
2026-09-20 00:06:01 +0700

    feat(test-group): implement test group business logic and API endpoints
    - Implement handling logic in TestGroupService
    - Expose endpoints in TestGroupController for retrieving test groups

--- Backend/src/main/java/com/youpass/backend/controller/TestGroupController.java
+++ Backend/src/main/java/com/youpass/backend/controller/TestGroupController.java
@@ -2,7 +2,10 @@ package com.youpass.backend.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/")
 public class TestGroupController {
 }
--- Backend/src/main/java/com/youpass/backend/service/TestGroupService.java
+++ Backend/src/main/java/com/youpass/backend/service/TestGroupService.java
@@ -1,8 +1,28 @@
 package com.youpass.backend.service;
 
 
+import com.youpass.backend.dto.response.TestGroupForReading;
+import com.youpass.backend.entity.ReadingPassage;
+import com.youpass.backend.entity.TestGroup;
+import com.youpass.backend.repository.TestGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TestGroupService {
+    @Autowired
+    private TestGroupRepository testGroupRepository;
+
+    public List<TestGroupForReading> getAllReadingFullTest() {
+        List<TestGroup> AllReadingFullTest = testGroupRepository.findBySkillType("reading");
+
+        return AllReadingFullTest.stream().filter(testGroup -> "reading".equalsIgnoreCase(testGroup.getSkillType()))
+                .map(testGroup -> TestGroupForReading.builder()
+                        .id(testGroup.getId())
+                        .title(testGroup.getTitle())
+                        .build()).toList();
+    }
+
 }
